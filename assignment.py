433a4720d1e1a2800a128e416f9b7507f5c91d4f
Daniel <2218766@student.uwtsd.ac.uk>
2024-01-16 19:44:46 +0000

    Added comments throughout program

--- assignment.py
+++ assignment.py
@@ -3,40 +3,56 @@ import requests
 
 
 def random_postcode():
+    # Requests the JSON file from the postcode API
+    # The API randomly selects a new file everytime the link is requested
     r = requests.get("https://api.postcodes.io/random/postcodes")
     data = r.json()
     data = data['result']
 
+    # Checks that the postcode generated has a valid latitude or longitude
+    # Not all postcodes have a latitude and longitude on the API
     if not data['longitude'] or not data['latitude']:
         return False
 
+    # Returns the data to the main function to use in the weather lookup function
     return data
 
 
 def user_postcode(input_postcode):
+    # Attempts to look up the user inputted postcode
     try:
+        # Concatenated the inputted postcode to the end of the api request
         r = requests.get("https://api.postcodes.io/postcodes/" + input_postcode)
         data = r.json()
         data = data['result']
+    # A key error will occur if an incorrect postcode is input
     except KeyError:
         error_message(3)
         return False
 
+    # Checks for valid longitude or latitude
     if not data['longitude'] or not data['latitude']:
         error_message(1)
         return False
 
+    # Returns the data to the main function to use in the weather lookup function
     return data
 
 
 def weather_lookup(postcode_data):
+    # Sets the latitude and longitude to string to concatenate onto the API URL
     latitude = str(postcode_data['latitude'])
     longitude = str(postcode_data['longitude'])
+
+    # Website URL split into two different variables to reduce single line code length
     website = "https://api.open-meteo.com/v1/forecast?"
     url = website+"latitude="+latitude+"&longitude="+longitude+"&current=temperature_2m,wind_speed_10m"
 
+    # Requests the weather JSON file from the Open Meteo API
     r = requests.get(url)
     data = r.json()
+
+    # Saves the units and data in two different variables to display in print info
     weather_units = data['current_units']
     weather_data = data['current']
 
@@ -58,6 +74,7 @@ def print_info(postcode_data, weather_data, weather_units):
 
 
 def error_message(number):
+    # Error message uses variable contained within the function to provide more useful messages
     print("\nError Message:", number)
     if number == 1:
         print("A postcode has been entered that does not have a valid latitude and longitude.")
@@ -76,34 +93,42 @@ def main():
     print("2. Generate a random postcode")
     user_input = input()
 
+    # Checks that a number has been entered
     try:
         user_input = int(user_input)
     except ValueError:
         error_message(2)
         return False
 
+    # Functionality for user inputted postcodes
     if user_input == 1:
         postcode_input = input("Please enter the postcode you would like to lookup: ")
         data = user_postcode(postcode_input)
+        # Forces the user to restart if there are any errors
         if not data:
             return False
         weather_lookup(data)
+    # Functionality for randomly generated postcodes
     elif user_input == 2:
         data = random_postcode()
         if not data:
             data = random_postcode()
         weather_lookup(data)
+    # Error checking when a wrong number is entered
     else:
         error_message(2)
         return False
 
+    # Functionality to loop the program without having to manually start running it again
     user_loop = input("\nWould you like to lookup another postcode? (y/n) ")
     print("")
+
     if user_loop == "y":
         return False
     return True
 
 
+# Loops the program until the user enters "y" in the main function
 loop = False
 while not loop:
     loop = main()
