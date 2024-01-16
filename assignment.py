827450b144c41e9bd4c3c8012ca0b234a286d75d
Daniel <2218766@student.uwtsd.ac.uk>
2024-01-16 17:31:35 +0000

    Testing api functionality

--- assignment.py
+++ assignment.py
@@ -1,4 +1,13 @@
 import json
+import requests
+
+
+def random_postcode():
+    r = requests.get("https://api.postcodes.io/random/postcodes")
+    data = r.json()
+    for postcode in data['result']:
+        print(postcode, " ", data['result'][postcode])
+
 
 with open('weather.json') as f:
     weatherData = json.load(f)
@@ -6,3 +15,7 @@ with open('weather.json') as f:
 for weatherConditions in weatherData['weather']:
     for key in weatherConditions:
         print("{0} : {1}".format(key, weatherConditions[key]))
+
+random_postcode()
+
+
