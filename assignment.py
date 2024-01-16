5c72698b072b513ed55369b6819377c4eca4f50c
Daniel <2218766@student.uwtsd.ac.uk>
2024-01-16 17:44:09 +0000

    Storing the useful info from the postcode api

--- assignment.py
+++ assignment.py
@@ -3,18 +3,33 @@ import requests
 
 
 def random_postcode():
+    postcode_data = {}
+    postcode_data['postcode'] = []
+    postcode_data['country'] = []
+    postcode_data['longitude'] = []
+    postcode_data['latitude'] = []
+    postcode_data['city'] = []
+
     r = requests.get("https://api.postcodes.io/random/postcodes")
     data = r.json()
-    for postcode in data['result']:
-        print(postcode, " ", data['result'][postcode])
+
+    postcode_data['postcode'] = data['result']['postcode']
+    postcode_data['country'] = data['result']['country']
+    postcode_data['longitude'] = data['result']['longitude']
+    postcode_data['latitude'] = data['result']['latitude']
+    postcode_data['city'] = data['result']['admin_district']
+
+    print(postcode_data)
 
 
-with open('weather.json') as f:
-    weatherData = json.load(f)
+def weather_info():
+    with open('weather.json') as f:
+        weather_data = json.load(f)
 
-for weatherConditions in weatherData['weather']:
-    for key in weatherConditions:
-        print("{0} : {1}".format(key, weatherConditions[key]))
+    for weatherConditions in weather_data['weather']:
+        for key in weatherConditions:
+            print("{0} : {1}".format(key, weatherConditions[key]))
+
 
 random_postcode()
 
